d4ec6daa683fb996b021ef06d82c11911d19e963
Graeme O <graeme.ong>
2017-07-09 00:51:49 +1000

    [GraemeO] Add Enzyme and simple React component test

---
+++ test/app.spec.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { expect } from 'chai';
+import { shallow, mount, render } from 'enzyme';
+import App from '../App';
+
+describe("App", function() {
+   it("should display Hello World", function() {
+     const wrapper = shallow(<App/>);
+     expect(wrapper.contains(<div>Hello World!!!</div>)).to.equal(true);
+   });
+});
