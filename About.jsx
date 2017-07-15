2f7a39d0535df8f68ee68f2c552aa9e353151d69
Graeme O <graeme.ong>
2017-07-16 03:18:24 +1000

    [GraemeO] Add input field, constructor, state handling to About component

--- About.jsx
+++ About.jsx
@@ -2,12 +2,27 @@ import React from 'react';
 
 
 class About extends React.Component {
+   constructor(props) {
+      super(props);
+
+      this.state = {
+         name:''
+      };
+    
+      this.handleChange = this.handleChange.bind(this);
+   }
+
+   handleChange(e) {
+      this.setState({
+         name: e.target.value 
+      });
+   }
 
    render() {
       return(
          <fieldset>
             <legend>About Yourself</legend>
-
+            <input type='text' id='name' name='name' value={this.state.name} onChange={this.handleChange} />
          </fieldset>
       );
    }
--- test/about.spec.js
+++ test/about.spec.js
@@ -9,4 +9,17 @@ describe('About', function() {
  
       expect(wrapper.contains(<legend>About Yourself</legend>)).to.equal(true);
    });
+
+   it('should find Name input field node', function() {
+      const wrapper = shallow(<About />);
+ 
+      expect(wrapper.find('#name').length).to.equal(1);
+   });
+
+   it('should display value from Name input field', function() {
+      const wrapper = shallow(<About />);
+
+      wrapper.find('#name').simulate('change', {target: {value: 'My Name'}});
+      expect(wrapper.find('#name').node.props.value).to.equal('My Name'); 
+   });
 });
