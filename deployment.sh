44b1e1166de3481cff42caa9739ec71bee8b50cc
Graeme O <graeme.ong>
2017-07-28 23:28:07 +0800

    [GraemeO] Update Ansible Galaxy roles version

--- deployment.sh
+++ deployment.sh
@@ -1,2 +1,2 @@
-ansible-galaxy install -r requirements.yml -p ./roles --force
+ansible-galaxy install -r requirements.yml -p ./roles --force -v
 ansible-playbook deployment.yml --extra-vars "host=$HOST user=$USER ssh_identity='$SSH_IDENTITY'" -vvv
