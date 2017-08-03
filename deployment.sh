62c657c132f22303b066ffb4204fa4fadc0e1287
Graeme O <graeme.ong>
2017-08-03 21:52:38 +0800

    [GraemeO] Add usage details for deployment script

--- deployment.sh
+++ deployment.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 function usage {
-   echo "Invalid parameter"
+   echo "Usage: $0 -h | --host -u | --user -i | --identity [-f | --force]"
+   echo "Where"
+   echo "    -h | --host is the host to which you will be configuring against"
+   echo "    -u | --user is the user to logon to the host"
+   echo "    -i | --identity is the SSH private key location to authenticate to the host"
+   echo "    (optional) -f | --force is the option to update downloaded Ansible role(s)"
    exit 1
 }
 
@@ -21,8 +26,11 @@ do
         shift
         ;;
      -f|--force)
-        force=true
+        force="--force"
         shift
+        ;;
+     --help)
+        usage
         ;;
      *)
         usage
@@ -30,10 +38,10 @@ do
    shift
 done
 
-echo $host
-echo $user
-echo $identity
-echo $force
+if [ -z $host ]  || [ -z $user ] || [ -z $identity ]
+then
+   usage   
+fi
 
-#ansible-galaxy install -r requirements.yml -p ./roles --force -v
+ansible-galaxy install -r requirements.yml -p ./roles $force -v
 #ansible-playbook deployment.yml --extra-vars "host=$host user=$user ssh_identity='$identity'" -vvv
