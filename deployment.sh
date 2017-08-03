06479c8b9909dab48e49275cd8f3691d3ab41b13
Graeme O <graeme.ong>
2017-08-03 21:02:44 +0800

    [GraemeO] Add Bash arguments handling for some sweetness

--- deployment.sh
+++ deployment.sh
@@ -1,2 +1,39 @@
-ansible-galaxy install -r requirements.yml -p ./roles --force -v
-ansible-playbook deployment.yml --extra-vars "host=$HOST user=$USER ssh_identity='$SSH_IDENTITY'" -vvv
+#!/bin/bash
+
+function usage {
+   echo "Invalid parameter"
+   exit 1
+}
+
+while [[ $# -gt 0 ]] 
+do
+   case $1 in
+     -h|--host) 
+        host=$2
+        shift
+        ;;
+     -u|--user)
+        user=$2
+        shift
+        ;;
+     -i|--identity)
+        identity=$2
+        shift
+        ;;
+     -f|--force)
+        force=true
+        shift
+        ;;
+     *)
+        usage
+   esac
+   shift
+done
+
+echo $host
+echo $user
+echo $identity
+echo $force
+
+#ansible-galaxy install -r requirements.yml -p ./roles --force -v
+#ansible-playbook deployment.yml --extra-vars "host=$host user=$user ssh_identity='$identity'" -vvv
