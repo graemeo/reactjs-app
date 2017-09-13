e85877e279fc86a77bb7111c970a01bcc30f2ee6
Graeme O <graeme.ong>
2017-09-13 19:23:51 +0800

    [GraemeO] Add and tidy up tests for Pokemon service

--- services/pokemon-service.js
+++ services/pokemon-service.js
@@ -5,7 +5,6 @@ export function getPokemons() {
    var url = config.pokemon.api.url;
    return request(url + '/api/v2/pokemon')
       .then(function(resp) {
-         console.log(JSON.parse(resp));
          return JSON.parse(resp);
       })
       .catch(function(err) {
--- test/pokemon-service.spec.js
+++ test/pokemon-service.spec.js
@@ -1,19 +1,20 @@
 import sinon from 'sinon';
 import proxyquire from 'proxyquire';
+import {expect} from 'chai';
 
 describe('Pokemon service', function() {
-   it('should return something', function() {
-      var config = {
-         pokemon: {
-            api: {
-               url: ''
-            }
-         }
-      };
+  var config = {
+     pokemon: {
+        api: {
+           url: ''
+        }
+     }
+  };
 
+  it('should return successful response when calling Pokemon API to get a list of Pokemon is successful', function() {
       var stub = sinon.stub();
 
-      stub.returns(Promise.resolve('{}'));
+      stub.returns(Promise.resolve('{"count": 1, "result": "bla"}'));
 
       var client = proxyquire('../services/pokemon-service', {
          'request-promise': stub,
@@ -21,8 +22,25 @@ describe('Pokemon service', function() {
       });
  
       return client.getPokemons()
-         .then(function(res){
-            console.log(res);
+         .then(function(response) {
+            expect(response.count).to.equal(1);
+            expect(response.result).to.equal("bla");
+         });
+   });
+
+   it('should return meaningful error message when calling Pokemon API to get a list of Pokemon fails', function() {
+      var stub = sinon.stub();
+
+      stub.returns(Promise.reject(new Error('I fail!')));
+ 
+      var client = proxyquire('../services/pokemon-service', {
+         'request-promise': stub,
+         'config': config
+      });
+
+      return client.getPokemons()
+         .then(function(response) {
+            expect(response).to.equal('not found');
          });
    });
 });
